493ab39f4bd7c1b29c6303f5ab7b770b5d795da1
Barry Thatcher <thatcher.barry@gmail.com>
2020-07-11 22:59:17 +0200

    'first'

---
+++ app.js
@@ -0,0 +1,18 @@
+var card = '';
+const cardContainer = document.querySelector('.cardContainer');
+
+function projectsList() {
+    projects.forEach(project=> {
+        card+=
+        `<div class="card">
+            <a href=${project.link}>
+            <img src=${project.image} alt="">
+            <h3>${project.name}</h3>
+            <p>${project.text}</p>
+            </a>
+        </div>`;
+        cardContainer.innerHTML = card;
+    });
+}
+
+projectsList();
---
+++ project_list.js
@@ -0,0 +1,56 @@
+const projects = [
+    {
+        name: "Change Background Colour",
+        text: "Click the button to change the colour",
+        image: "https://www.finnfestivalcanada.com/wp-content/uploads/2020/06/click.jpg",
+        link: "#",
+    },
+    {
+        name: "Change Hex Colour",
+        text: "Click the button to change the hex colour",
+        image: "https://pbs.twimg.com/profile_images/1061975250819108864/aKQLW7DB_400x400.jpg",
+        link: "#",
+    },
+    {
+        name: "Random Quotes",
+        text: "Generate random quotes to grow wise",
+        image: "https://marketplace.magento.com/media/catalog/product/cache/603f9e977a3dc35468ba3ae89ddfbb29/r/a/randquote_1_1.png",
+        link: "#",
+    },
+    {
+        name: "Pass the Message",
+        text: "Write up a message and pass it on",
+        image: "https://icon-library.com/images/messages-icon-png/messages-icon-png-2.jpg",
+        link: "#",
+    },
+    {
+        name: "Just a Counter",
+        text: "Add or subtract from the counter",
+        image: "https://lh3.googleusercontent.com/dvPnZK6yR0kw-nM0Vfcy7lNDmdYS50QMCa7318emsFIieB7AtbdJC0ykSv1tczw6udA",
+        link: "#",
+    },
+    {
+        name: "Image Slider",
+        text: "Just another image slider",
+        image: "https://smartslider3.com/wp-content/uploads/2019/01/imageslider-1-780x410.png",
+        link: "#",
+    },
+    {
+        name: "Testimonial Page",
+        text: "Fun star wars testimonal page",
+        image: "https://econsultancy.imgix.net/content/uploads/2018/10/02143756/iStock-941187660.jpg",
+        link: "#",
+    },
+    {
+        name: "Filter Items",
+        text: "Check out grandma's treat shop",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR73I8WT0zaAND2onmzIyfyjcQuhw2qoQqv7g&usqp=CAU",
+        link: "#",
+    },
+    {
+        name: "Calculator",
+        text: "A simple calculator project",
+        image: "https://www.princetonreview.com/cms-content/CanYouUseCalculatoronGMAT.jpg",
+        link: "#",
+    },
+]
